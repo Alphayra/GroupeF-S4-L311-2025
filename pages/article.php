635d3ce9d47e818893f1a2dc2fb07086664743f4
Alphyra <109241775+Alphayra@users.noreply.github.com>
2025-10-26 19:10:55 +0100

    📝 Commentaires article.php

--- pages/article.php
+++ pages/article.php
@@ -1,13 +1,17 @@
 <?php
+
+    #Récupération des données de l'article par la variable GET
 	$article = getArticleById(
 		array_key_exists('id', $_GET) ? $_GET['id'] : null
 	);
 
-	if(is_null($article) OR !count($article)){
-		header('Location:index.php');
-        exit;
-	}
-?>	
+    # S'il n'y a pas d'article, revenir à index.php
+if(is_null($article) OR !count($article)){
+    header('Location:index.php');
+    exit;
+}
+    # Sinon afficher l'article
+?>
 <section class="banner style1 orient-left content-align-left image-position-right fullscreen onload-image-fade-in onload-content-fade-right">
 	<div class="content">
 		<h1><?php echo $article['titre'];?></h1>
