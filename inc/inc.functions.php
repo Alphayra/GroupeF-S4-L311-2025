8e1dce93be213683478d6ffa261b0ea6e588623c
Alphyra <109241775+Alphayra@users.noreply.github.com>
2025-10-26 19:12:56 +0100

    📝 Commentaires inc.functions.php

--- inc/inc.functions.php
+++ inc/inc.functions.php
@@ -6,9 +6,11 @@
     define('PASSWORD', 'U31311');
     define('DB_ARTICLES', TL_ROOT.'/db/articles.json');
 
+    # Vérification de si l'utilisateur est connecté
     function connectUser($login = null, $password = null){
         if(!is_null($login) && !is_null($password)){
             if($login === LOGIN && $password === PASSWORD){
+                #Si il est connecté avec les bons logins, retour d'un tableau avec login et password
                 return array(
                     'login'    => LOGIN,
                     'password' => PASSWORD
@@ -18,11 +20,13 @@
         return null;
     }
 
+    #Déconnecte l'utilisateur
     function setDisconnectUser(){
          unset($_SESSION['User']);
          session_destroy();
     }
 
+    #Vérifie si l'utilisateur est connecté en retournant "true" ou "false"
     function isConnected(){
         if(array_key_exists('User', $_SESSION) 
                 && !is_null($_SESSION['User'])
@@ -32,6 +36,8 @@
         return false;
     }
 
+    # Inclut le fichier de template là ou la fonction est appelé,
+    # Si $page est nul, importe index.php par défaut à la place.
     function getPageTemplate($page = null){
         $fichier = TL_ROOT.'/pages/'.(is_null($page) ? 'index.php' : $page.'.php');
 
@@ -42,6 +48,8 @@
         }
     }
 
+    # Récupère tout les articles depuis un fichier json,
+    # Si le fichier existe, retourne ce dernier après l'avoir passé dans json_decode, sinon rien.
     function getArticlesFromJson(){
         if(file_exists(DB_ARTICLES)) {
             $contenu_json = file_get_contents(DB_ARTICLES);
@@ -51,6 +59,7 @@
         return null;
     }
 
+    # Vérifie l'existence d'un article et si c'est le cas, le renvoi par la fonction
     function getArticleById($id_article = null){
        if(file_exists(DB_ARTICLES)) {
             $contenu_json = file_get_contents(DB_ARTICLES);
